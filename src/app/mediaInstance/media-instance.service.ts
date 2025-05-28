2397afc1a7f9afe9a083bbc4e592164117902f68
RealSlimKlewus <slimklewus@gmail.com>
2025-05-28 10:24:28 +0200

    Changed default value

--- src/app/mediaInstance/media-instance.service.ts
+++ src/app/mediaInstance/media-instance.service.ts
@@ -10,14 +10,14 @@ export class MediaInstanceService {
     mediumType: 'series',
     segment: {
       book: 1,
-      chapter: 2 
+      chapter: 1
 
     }
   })
 
   constructor() { }
 
-  
+
 
   getMediaInstance() {
     return this.currentMediaInstance
@@ -31,7 +31,7 @@ export class MediaInstanceService {
     const selectedInstance = mediaInstances.find((instance) => instance.mediumType === this.currentMediaInstance().mediumType)
 
     if(!selectedInstance || selectedInstance.segment.book > this.currentMediaInstance().segment.book ) return true
-    
+
 
     if(selectedInstance.segment.book == this.currentMediaInstance().segment.book && selectedInstance.segment.chapter > this.currentMediaInstance().segment.chapter ) return true
 
